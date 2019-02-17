05494e36de7368440d43b1a0a03a39aa48e9f50b
Martyn O'Connor <contact@martynoconnor.net>
2019-02-17 23:25:25 +0000

    Update babyshark.py

--- babyshark.py
+++ babyshark.py
@@ -1,3 +1,3 @@
 #!/usr/bin/env python
 print("\n".join(list(map(lambda x: ((x + " shark " + "doo! " * 6).rstrip() + "\n") * 3 + x + " shark!\n",
-                         [i for i in ["Baby", "Daddy", "Mommy", "Grandpa", "Grandma"]]))).strip())
+                         [i for i in ["Baby", "Mommy", "Daddy", "Grandma", "Grandpa"]]))).strip())
